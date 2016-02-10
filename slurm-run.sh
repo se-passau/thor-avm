4e1c0b10bdb079ec15e6304df6a9e5fdfe726e7b
Stefan Sobernig <stefan.sobernig@wu.ac.at>
2016-02-10 15:48:30 +0100

    Got sbatch params for job scheduling

--- slurm-run.sh
+++ slurm-run.sh
@@ -5,22 +5,23 @@ SPL=$2
 USER=sobernig
 PREFIX=/home/${USER}
 
+LOCAL=/local/${USER}/${SLURM_JOB_ID}
 ## staging to nodes
-cd /local/$USER
-cp -R ${PREFIX}/sayyad .
-cd sayyad
-## trigger the run (assuming `ant compile` was performed once on the SLURM server: debussy).
-ant -Dalgo=$1 -Dspl=$2 -Drepeats=1 run
+cp -R ${PREFIX}/sayyad ${LOCAL}
+(
+    cd ${LOCAL}
+    ## trigger the run
+    ## (assuming `ant compile` was performed once on the SLURM server: debussy).
+    ant -Dalgo=${ALGO} -Dspl=${SPL} -Drepeats=1 run
+    
+    ## copy-merge the result data back to the SLURM server 
+    for file in $(find NSGAIIDMStudy/data -type f); do
+	## echo "${file}"
+	mkdir -p "${PREFIX}/$(dirname "${file}")"
+	cat "${file}" >> "${PREFIX}/${file}"
+    done
+)
 
-## copy-merge the result data back to the SLURM server
-
-for file in $(find NSGAIIDMStudy/data -type f); do
-    echo "${file}"
-    mkdir -p "${PREFIX}/$(dirname "${file}")"
-    cat "${file}" >> "${PREFIX}/${file}"
-done
-
-## provide a clean state
-cd ..
-rm -rf sayyad/
+## provide a clean slate
+rm -rf ${LOCAL}
 
