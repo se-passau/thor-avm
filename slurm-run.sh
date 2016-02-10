2fb33e3cceec4961dd8a2465eb0d2a2a3e34f012
Stefan Sobernig <stefan.sobernig@wu.ac.at>
2016-02-10 13:37:38 +0100

    Provide slurm run script

---
+++ slurm-run.sh
@@ -0,0 +1,26 @@
+ALGO=$1
+SPL=$2
+
+USER=sobernig
+PREFIX=/home/${USER}
+
+## staging to nodes
+cd /local/$USER
+mkdir sayyad
+cd sayyad
+
+## trigger the run (assuming `ant compile` was performed once on the SLURM server: debussy).
+ant -Dalgo=$1 -Dspl=$2 -Drepeats=1 run
+
+## copy-merge the result data back to the SLURM server
+
+for file in $(find NSGAIIDMStudy/data -type f); do
+    echo "${file}"
+    mkdir -p "${PREFIX}/$(dirname "${file}")"
+    cat "${file}" >> "${PREFIX}/${file}"
+done
+
+## provide a clean state
+cd ..
+rm -rf sayyad/
+
