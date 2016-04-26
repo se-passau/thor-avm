eafe46d5f70e2e2c0d3bd5c670f0b5c149444dcc
Stefan Sobernig <stefan.sobernig@wu.ac.at>
2016-04-26 14:53:50 +0200

    Restructured interaction-aware code to limit overhead

--- src/jmetal/problems/dimacs/Ptoybox2.java
+++ src/jmetal/problems/dimacs/Ptoybox2.java
@@ -2,30 +2,27 @@ package jmetal.problems.dimacs;
 
 import jmetal.encodings.variable.Binary;
 
-/* TODO: relocate */
 import com.opencsv.CSVReader;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.util.List;
 import java.io.IOException;
 import java.util.BitSet;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Ptoybox2 extends Ptoybox {   
 
-    public final Map <BitSet, Double> interactions = new HashMap<BitSet, Double>();
-    
-    public Ptoybox2(String solutionType) throws ClassNotFoundException, IOException {
-	super(solutionType);
+    public final static List<Entry<BitSet, Double>> interactions =
+	new ArrayList<Entry<BitSet, Double>>();
 
-	/* TODO relocate later, make static */
+    static {
 	String interactFile = Ptoybox.class.getResource("/dimacs/ivmI16.csv").getPath();
 	try (CSVReader reader =
 	     new CSVReader(new BufferedReader(new FileReader(interactFile)), ',', '"', 1);
 	     ) {
-	  
 	    List<String[]> lines = reader.readAll();
 	    for (String[] line : lines) {
 		int numOfFeatures = line.length-1;
@@ -36,20 +33,22 @@ public class Ptoybox2 extends Ptoybox {
 			bs.set(i);
 		    }
 		}
-		this.interactions.put(bs, Double.parseDouble(line[line.length-1]));
-	    }
+		Entry e = new SimpleImmutableEntry(bs, Double.parseDouble(line[line.length-1]));
+		interactions.add(e);
+	    } 
 	} catch (Exception e) {
 	    System.out.println("OpenCVS: " + e.getMessage());
-	    throw e;
 	}
+    }
+    
+    public Ptoybox2(String solutionType) throws ClassNotFoundException {
+	super(solutionType);
     }
 
     public double computeCosts(Binary variable) {
 	double cost = super.computeCosts(variable);
-	
-	/* TODO: make it a simple list */
-	
-	for (Map.Entry<BitSet, Double> interactEntry : this.interactions.entrySet()) {
+
+	for (Entry<BitSet, Double> interactEntry : this.interactions) {
 	    /* Is interaction a subset of the solution:
 	       solution & interaction = interaction? */
 	    BitSet interaction = interactEntry.getKey();
