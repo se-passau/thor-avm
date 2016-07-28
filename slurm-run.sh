3e25acadf77557b37c34f8eff4cd4aa2a7806dfc
Stefan Sobernig <stefan.sobernig@wu.ac.at>
2016-07-28 13:33:08 +0200

    Make sure nodes don't step on each others' feet when writing back results

--- slurm-run.sh
+++ slurm-run.sh
@@ -16,17 +16,23 @@ cp -R ${PREFIX}/sayyad ${LOCAL}
     ## trigger the run
     ## (assuming `ant compile` was performed once on the SLURM server: debussy).
     ant -Dalgo=${ALGO} -Dspl=${SPL} -Drepeats=1 -Ddatabed=${DATABED} -Dname=${DIR} run
+
+    mkdir -p "${PREFIX}/$(dirname "${file}")"
     
     ## copy-merge the result data back to the SLURM server 
     for file in $(find ${DIR}/data -type f ! -name "FUN.*" ! -name "VAR.*"); do
 	## echo "${file}"
-	mkdir -p "${PREFIX}/$(dirname "${file}")"
-	cat "${file}" >> "${PREFIX}/${file}"
+	## mkdir -p "${PREFIX}/$(dirname "${file}")"
+	(
+	    flock -n 100 || exit 1
+	    cat "${file}" >> "${PREFIX}/${file}"
+	    
+	) 100>${PREFIX}/.slurmlock
     done
 
     for file in $(find ${DIR}/data -type f -name "FUN.*" -o -name "VAR.*"); do
 	## echo "${file}"
-	mkdir -p "${PREFIX}/$(dirname "${file}")"
+	## mkdir -p "${PREFIX}/$(dirname "${file}")"
 	cat "${file}" > "${PREFIX}/${file}.${SLURM_JOB_ID}"
     done
 )
